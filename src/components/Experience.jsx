52b8d1b9f537413c538a9ca1db8fe72e112e0098
saudkhan <saudkhanbpk@gmail.com>
2025-02-07 09:24:12 +0500

    experince updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -33,7 +33,7 @@ function Experience() {
                             <a className='font-light text-gray-400 self-center'>Swabi, KPK, Pakistan</a>
                         </div>
                     </div>
-                    <p className='text-xl italic text-gray-400 mb-4'>January 2023 to Present</p>
+                    <p className='text-xl italic text-gray-400 mb-4'>January 2023 to January 2025</p>
                     <motion.p
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
@@ -41,7 +41,32 @@ function Experience() {
                         whileHover={{ scale: 1.05 }}
                         className='text-gray-400 text-xl font-light'
                     >
-                        Since January 2023, I've been deeply involved in full-stack development using the MERN stack. I have a strong foundation in setting up and managing backend services with Node.js and Express, and I am particularly confident in my frontend development skills with React. My expertise includes creating dynamic and responsive user interfaces, ensuring seamless integration with backend APIs, and delivering high-quality digital experiences. I thrive in collaborative environments and stay updated with the latest industry trends to continuously improve my skills and contribute to innovative projects.
+                        From January 2023 to August 2024, I was involved in full-stack development using the MERN stack. I managed backend services with Node.js and Express and excelled in frontend development with React. I created dynamic and responsive user interfaces, ensured seamless API integration, and delivered high-quality digital experiences.
+                    </motion.p>
+                </motion.div>
+
+                <motion.div
+                    initial={{ opacity: 0, x: -50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 2 }}
+                    className='relative md:ml-10 mt-8'
+                >
+                    <div className='flex flex-wrap gap-2 items-center mb-4'>
+                        <p className='text-xl p-1 font-semibold italic text-yellow-200'>IDEATION</p>
+                        <div className='flex gap-1'>
+                            <FaLocationArrow className='self-center text-gray-400' />
+                            <a className='font-light text-gray-400 self-center'>Islamabad, Punjab, Pakistan</a>
+                        </div>
+                    </div>
+                    <p className='text-xl italic text-gray-400 mb-4'>February 2025 to Present</p>
+                    <motion.p
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 1, delay: 2.5 }}
+                        whileHover={{ scale: 1.05 }}
+                        className='text-gray-400 text-xl font-light'
+                    >
+                        Since February 2025, I have been working as a frontend developer at Ideation. My role involves creating dynamic and responsive user interfaces using modern web technologies. I collaborate closely with the design and backend teams to ensure seamless integration and deliver high-quality digital experiences.
                     </motion.p>
                 </motion.div>
             </div>
